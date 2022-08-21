1cb760362d64e404825c0095443927c0d6d7c1df
jjjoh <45453960+jjjoh@users.noreply.github.com>
2022-08-21 15:00:32 +0200

    modify the cv adress in myButtonLink

--- script/main.js
+++ script/main.js
@@ -32,7 +32,7 @@ function myButtonLink(selecteur, contenu, adresse) {
 }
 
 myButtonLink(".contactButton", "contact", "./contact.html" );
-myButtonLink(".cvButton", "cv", "./cvJohanneSchatz.pdf" );
+myButtonLink(".cvButton", "cv", "./cvJohanneSCHATZ_11-08-2022.pdf" );
 
 
 
